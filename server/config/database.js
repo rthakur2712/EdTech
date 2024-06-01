9891bc0f1b23369965154a210857507213dbf36b
sachinthakur1912 <thakursachin1912@gmail.com>
2024-06-01 13:30:13 +0530

    models complete

---
+++ server/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+exports.connect = () =>{
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log("DB connection successful")
+    })
+    .catch((error)=>{
+        console.log("error in db connectioin");
+        console.error(error);
+        process.exit(1);
+    }
+    )
+       
+    
+};
