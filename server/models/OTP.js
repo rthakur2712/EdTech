f7430eb33890addbe8b7094bdc8497b2bce98c82
sachinthakur1912 <thakursachin1912@gmail.com>
2024-06-14 22:37:17 +0530

    authentication controllers complete

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mailSender = require('../utils/mailSender');
 
 const OTPSchema = new mongoose.Schema({
     email:{
@@ -15,5 +16,20 @@ const OTPSchema = new mongoose.Schema({
         expires:5*60
     }
 });
+// function to send mails
+async function sendVerificationEmail(email,otp){
+    try{
+        const mailResponse = await mailSender(email, "Verification mail from Study Notion",otp)
+        console.log("Email sent successfully",mailResponse)
+    }catch(error){
+        console.log("Error occured while sending mail : ",error);
+        throw error;
+    }
+}
+
+OTPSchema.pre("save",async function(next){
+    await sendVerificationEmail(this.email,this.otp);
+    next();
+})
 
 module.exports = mongoose.model("OTP",OTPSchema);
